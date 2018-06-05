dd98c01a9fbe058e5b53f24cc6fab920401c8a4b
Carlos Loeza <c.loeza1116@gmail.com>
2018-06-05 05:38:50 -0700

    Fixed all of the text fields(keyboards)
    All keyboars now proceed to the next text field or hide keyboard if it's already at the last one

--- SettingsPage/InstagramSignIn.swift
+++ SettingsPage/InstagramSignIn.swift
@@ -1,37 +1,35 @@
 //
-//  WeedmapsSignInViewController.swift
+//  InstagramSignIn.swift
 //  SettingsPage
 //
-//  Created by Carlos on 6/4/18.
+//  Created by Carlos on 6/5/18.
 //  Copyright © 2018 Carlos. All rights reserved.
 //
 
 import UIKit
 
-class WeedmapsSignIn: UIViewController, UITextFieldDelegate {
+class InstagramSignIn: UIViewController, UITextFieldDelegate {
 
-    // User textField
+    // User text field
     @IBOutlet weak var User: UITextField!
-    // Password textField
+    // Password text field
     @IBOutlet weak var Password: UITextField!
     
     
     override func viewDidLoad() {
-
         User.delegate = self
         Password.delegate = self
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    // Hides keyboard when user clicks outside of keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with: UIEvent?){
         self.view.endEditing(true)
     }
     
-    
+    // When user clicks return it proceeds to the next textField or
+    // exits if it's at the passworrd textField
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         if textField == User {
             Password.becomeFirstResponder()
         } else if textField == Password{
@@ -39,6 +37,8 @@ class WeedmapsSignIn: UIViewController, UITextFieldDelegate {
         }
         return true
     }
+    
+    
 
     /*
     // MARK: - Navigation
